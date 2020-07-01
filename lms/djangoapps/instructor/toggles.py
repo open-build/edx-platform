88fbc9f04e979aba137daf739c90c50f7b2860f2
Ahtisham Shahid <ahtisham300@gmail.com>
2020-07-01 12:14:21 +0500

    Break down Html and fixed tests

--- lms/djangoapps/instructor/toggles.py
+++ lms/djangoapps/instructor/toggles.py
@@ -2,19 +2,19 @@
 Waffle flags for instructor dashboard.
 """
 
-from openedx.core.djangoapps.waffle_utils import WaffleFlag, WaffleFlagNamespace
+from openedx.core.djangoapps.waffle_utils import CourseWaffleFlag, WaffleFlagNamespace
 
 WAFFLE_NAMESPACE = 'instructor'
 
-DATA_DOWNLOAD_V2 = WaffleFlag(
-    waffle_namespace=WaffleFlagNamespace(name=WAFFLE_NAMESPACE, log_prefix=u'instructor_dashboard: '),
+DATA_DOWNLOAD_V2 = CourseWaffleFlag(
+    waffle_namespace=WaffleFlagNamespace(name=WAFFLE_NAMESPACE, log_prefix='instructor_dashboard: '),
     flag_name='enable_data_download_v2',
     flag_undefined_default=False
 )
 
 
-def data_download_v2_is_enabled():
+def data_download_v2_is_enabled(course_key):
     """
     check if data download v2 is enabled.
     """
-    return DATA_DOWNLOAD_V2.is_enabled()
+    return DATA_DOWNLOAD_V2.is_enabled(course_key)
